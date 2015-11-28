ef469bc95a8f18c465cc524ada01f52c73dac952
lucianofedericoolmedo <luciano.federico.olmedo@gmail.com>
2015-11-28 18:38:13 -0300

    add drawables

---
+++ app/src/main/java/grupo6uis/dueloentreleyendasfinal/dueloApp/EstadisticasActivity.java
@@ -0,0 +1,9 @@
+package grupo6uis.dueloentreleyendasfinal.dueloApp;
+
+/**
+ * Created by luciano on 28/11/15.
+ */
+public class EstadisticasActivity {
+
+    public static final String PERSONAJE_ID = "id";
+}
